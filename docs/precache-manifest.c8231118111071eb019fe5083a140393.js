2a23478799e680a70dc1a119d47f06261e231db0
Aubrey <aubrey.snider@gmail.com>
2020-06-30 13:29:10 -0500

    add live demo

---
+++ docs/precache-manifest.c8231118111071eb019fe5083a140393.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0e7110d6b221516d1458f9ec73cdd74",
+    "url": "/banana-grabber/index.html"
+  },
+  {
+    "revision": "6b35a232a8f8126eadc0",
+    "url": "/banana-grabber/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "445bb46c654f908536d7",
+    "url": "/banana-grabber/static/css/main.64d8bcc1.chunk.css"
+  },
+  {
+    "revision": "6b35a232a8f8126eadc0",
+    "url": "/banana-grabber/static/js/2.9932cd5b.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/banana-grabber/static/js/2.9932cd5b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "445bb46c654f908536d7",
+    "url": "/banana-grabber/static/js/main.1d6c5777.chunk.js"
+  },
+  {
+    "revision": "a0968c18ff9758e7572d",
+    "url": "/banana-grabber/static/js/runtime-main.05ba1750.js"
+  },
+  {
+    "revision": "cf4583822a548069d09e26ff7afede02",
+    "url": "/banana-grabber/static/media/banana100.cf458382.png"
+  }
+]);
